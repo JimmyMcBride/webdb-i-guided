b840bbf0a8852a64da5f38b6f92cbca17083983b
JimmyMcBride <mcbride967@gmail.com>
2019-08-06 23:14:12 -0500

    finished guided project

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,28 +1,95 @@
-const express = require('express');
+const express = require('express')
 
-// database access using knex
-const db = require('../data/db-config.js');
+// database access using knex 👊
+const db = require('../data/db-config.js')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    // const posts = await db('posts')
+    const posts = await db.select('*').from('posts')
+    res.status(200).json(posts)
+  } catch (err) {
+    res.status(500).json({
+      message: 'Error getting posts ☠️', error: err
+    })
+  }
+})
 
-});
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    // const [post] = await db('posts').where({ id })
+    const [post] = await db.select('*').from('posts').where({ id })
+    if (post) {
+      res.status(200).json(post)
+    } else {
+      res.status(404).json({
+        message: 'Could not find post id 🤷‍'
+      })
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'Error getting post ☠️', error: err
+    })
+  }
+})
 
-router.get('/:id', (req, res) => {
+router.post('/', async (req, res) => {
+  const postData = req.body
+  try {
+    const post = await db('post').insert(postData)
+    if (post) {
+      res.status(201).json(post)
+    }
+  } catch (err) {
+      res.status(500).json({
+        message: 'Could not add your post to the data base 💩',
+        error: err
+      })
+  }
+})
 
-});
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const changes = req.body
+  try {
+    const count = await db('posts').where('id', '=', id).update(changes)
+    if (count) {
+      res.status(200).json(count)
+    } else {
+      res.status(400).json({
+        message: 'Could not update the post 💩'
+      })
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'Could not update the post 💩',
+      error: err
+    })
+  }
+})
 
-router.post('/', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  const post = req.body
+  try {
+    const count = await db('posts').where('id', '=', id).delete(post)
+    if (count > 0) {
+      res.status(200).json({
+        message: 'This post has been destroyed ☠️'
+      })
+    } else {
+      res.status(404).json({
+        message: 'The post you are looking to delete could not be found 🤷‍'
+      })
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'Destruction of post was unsuccessful 💩'
+    })
+  }
+})
 
-});
-
-router.put('/:id', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-module.exports = router;
+module.exports = router
